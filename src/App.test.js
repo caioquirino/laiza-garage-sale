2776a672fb5dbbbf1a9cce784289eb3e6a0c5585
Matheus Moreira <mattelias@live.com>
2023-01-30 01:45:17 +0100

    tests: add mock apollo provider

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from "@testing-library/react";
+import { MockedProvider } from "@apollo/react-testing";
 import App from "./App";
 
 test("renders garage", () => {
-  render(<App />);
+  render(
+    <MockedProvider>
+      <App />
+    </MockedProvider>
+  );
+
   const garage = screen.getByText(/garage/i);
   expect(garage).toBeInTheDocument();
 });
